f1dc243d1d51c73f4a73c5d60d4b587e9b97a49e
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-18 17:51:23 -0500

    Create item.py

---
+++ rpg/item.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name: str, type: str, description: str, value: int) -> None:
+        self.name = name
+        self.type = type
+        self.description = description
+        self.value = value
