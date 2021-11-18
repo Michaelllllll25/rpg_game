1f59ea3a3f5b4e47b8ba2058a36e7a75d451e948
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-18 17:50:00 -0500

    Update battle.py

--- rpg/battle.py
+++ rpg/battle.py
@@ -1,1 +1,23 @@
+import random
+from typing import List
+
+from .character import Character
+
+
+class Battle:
+    def __init__(self, player_party: List[Character],
+                 enemy_party: List[Character]) -> None:
+        self._player_party = player_party
+        self._enemy_party = enemy_party
+        self._turn = 0
+        self._turn_order = []
+        self._init_order()
+    
+    def _init_order(self) -> None:
+        self._turn_order = self._player_party + self._enemy_party
+        random.shuffle(self._turn_order)
+    
+    def get_current_character(self) -> Character:
+        character_index = self._turn % len(self._turn_order)
+        return self._turn_order[character_index]
 
