1ee90f85c7c6ae3ba2d85c9f64c451e65bee4cac
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-19 01:42:25 -0500

    Update magic.py

--- rpg/magic.py
+++ rpg/magic.py
@@ -1,2 +1,13 @@
+import random
+
 class Spell:
-    def __init__(self)
+    def __init__(self, name, dmg, cost):
+        self.name = name
+        self.dmg = dmg
+        self.cost = cost
+
+
+    def generate_damage(self):
+        low = self.dmg - 15
+        high = self.dmg + 15
+        return random.randrange(low, high)  
