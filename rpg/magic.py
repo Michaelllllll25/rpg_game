63eb72de53b74c5b998496a85dee009c5b9a52f5
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-11-21 15:12:39 -0500

    Update magic.py

--- rpg/magic.py
+++ rpg/magic.py
@@ -1,15 +1,17 @@
 import random
 
 class Spell:
-    def __init__(self, name, dmg, cost):
+    def __init__(self, name: str, dmg: int, cost: int) -> None:
         self.name = name
         self.dmg = dmg
         self.cost = cost
 
     def generate_damage(self):
+        """Generates a damage amount
+        
+        Returns:
+            A number between -15 and 15
+        """
         low = self.dmg - 15
         high = self.dmg + 15
         return random.randrange(low, high)  # gets a number that is in the range of -15 and 15
-
-
- 
